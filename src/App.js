ac7d4102e11021c348c88153423e819fcf8b35f1
Jeffrey Liu <jeffkliu@gmail.com>
2023-01-23 19:18:27 -0500

    Generating Random ID's

--- src/App.js
+++ src/App.js
@@ -6,11 +6,19 @@ function App () {
     const [books, setBooks] = useState([]);
 
     const createBook = (title) => {
-        console.log('Need to add book with: ', title);
+        const updatedBooks = [
+            ...books, 
+            {
+                id: Math.round(Math.random() * 9999), 
+                title
+            },
+        ];
+        setBooks(updatedBooks);
     };
 
     return (
         <div>
+            {books.length}
             <BookCreate onCreate={createBook} />
         </div>
     );
