d647cbc98c0142ab76a931e339e18d71a829947d
Jeffrey Liu <jeffkliu@gmail.com>
2023-01-24 19:09:40 -0500

    Creating a New Record

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,8 +1,7 @@
 import BookShow from "./BookShow";
 
 function BookList({ books, onDelete, onEdit }) {
-
-
+    
     const renderedBooks = books.map((book) => {
         return <BookShow onEdit={onEdit} onDelete={onDelete} key={book.id} book={book} />;
     });
